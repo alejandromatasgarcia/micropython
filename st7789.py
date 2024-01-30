2ee7ffaad224c04ddc054f0bb75c2d60172e916a
Alejandro Matás García <144593715+alejandromatasgarcia@users.noreply.github.com>
2024-01-30 19:52:40 +0100

    Add files via upload

--- st7789.py
+++ st7789.py
@@ -1,7 +1,7 @@
 from machine import Pin,SPI,PWM
 import framebuf
 
-# asignación de pines
+# pin asignment
 BL = const(13)
 DC = const(8)
 RST = const(12)
@@ -10,21 +10,22 @@ SCK = const(10)
 CS = const(9)
 
 class LCD_1inch14(framebuf.FrameBuffer):
-    # esta clase hereda de FrameBuffer
-    def __init__(self,rotation=0,brightness=16384):
-        # añadimos rotación
+    
+    def __init__(self, rotation:int=0, brightness:int=16384):
+        # add rotation
         if rotation in (0,90,180,270):
             self.rotation = rotation
         else:
             self.rotation = 0
-        self.rot0180 = (self.rotation == 0 or self.rotation == 180)
-        if self.rot0180:
+        if (self.rotation == 0 or self.rotation == 180):
             self.width = 240
             self.height = 135
         else:
             self.width = 135
             self.height = 240
-        # las 2 rotaciones "que faltan" las haremos haciendo mirror de X e Y
+        # the 2 "missing" rotations will be done mirroring X & Y
+        
+        self.brightness = brightness
         
         self.cs = Pin(CS,Pin.OUT)
         self.rst = Pin(RST,Pin.OUT)
@@ -41,35 +42,35 @@ class LCD_1inch14(framebuf.FrameBuffer):
         
         self.pwm = PWM(Pin(BL))
         self.pwm.freq(1000)
-        self.brillo(brightness)
+        self.set_brightness(self.brightness)
         
-        # teclas.
+        # Waveshare 1.14" LCD keys
         self.keyA = Pin(15,Pin.IN,Pin.PULL_UP) # A
         self.keyB = Pin(17,Pin.IN,Pin.PULL_UP) # B
-        self.keyC = Pin(3,Pin.IN,Pin.PULL_UP) # centro
-        # Las definimos según la rotación de la pantalla
+        self.keyC = Pin(3,Pin.IN,Pin.PULL_UP) # joystick center
+        # joystick keys are defined according to screen rotation
         if self.rotation == 0:
-            self.keyUP = Pin(2,Pin.IN,Pin.PULL_UP) # arriba
-            self.keyLEFT = Pin(16,Pin.IN,Pin.PULL_UP) # izquierda
-            self.keyDOWN = Pin(18,Pin.IN,Pin.PULL_UP) # abajo
-            self.keyRIGHT = Pin(20,Pin.IN,Pin.PULL_UP) # derecha
+            self.keyUP = Pin(2,Pin.IN,Pin.PULL_UP)
+            self.keyLEFT = Pin(16,Pin.IN,Pin.PULL_UP)
+            self.keyDOWN = Pin(18,Pin.IN,Pin.PULL_UP)
+            self.keyRIGHT = Pin(20,Pin.IN,Pin.PULL_UP)
         elif self.rotation == 90:
-            self.keyUP = Pin(20,Pin.IN,Pin.PULL_UP) # arriba
-            self.keyLEFT = Pin(2,Pin.IN,Pin.PULL_UP) # izquierda
-            self.keyDOWN = Pin(16,Pin.IN,Pin.PULL_UP) # abajo
-            self.keyRIGHT = Pin(18,Pin.IN,Pin.PULL_UP) # derecha
+            self.keyUP = Pin(20,Pin.IN,Pin.PULL_UP)
+            self.keyLEFT = Pin(2,Pin.IN,Pin.PULL_UP)
+            self.keyDOWN = Pin(16,Pin.IN,Pin.PULL_UP)
+            self.keyRIGHT = Pin(18,Pin.IN,Pin.PULL_UP)
         elif self.rotation == 180:
-            self.keyUP = Pin(18 ,Pin.IN,Pin.PULL_UP) # arriba
-            self.keyLEFT = Pin(20 ,Pin.IN,Pin.PULL_UP) # izquierda
-            self.keyDOWN = Pin(2 ,Pin.IN,Pin.PULL_UP) # abajo
-            self.keyRIGHT = Pin(16 ,Pin.IN,Pin.PULL_UP) # derecha
+            self.keyUP = Pin(18 ,Pin.IN,Pin.PULL_UP)
+            self.keyLEFT = Pin(20 ,Pin.IN,Pin.PULL_UP)
+            self.keyDOWN = Pin(2 ,Pin.IN,Pin.PULL_UP)
+            self.keyRIGHT = Pin(16 ,Pin.IN,Pin.PULL_UP)
         elif self.rotation == 270:
-            self.keyUP = Pin(16,Pin.IN,Pin.PULL_UP) # arriba
-            self.keyLEFT = Pin(18,Pin.IN,Pin.PULL_UP) # izquierda
-            self.keyDOWN = Pin(20,Pin.IN,Pin.PULL_UP) # abajo
-            self.keyRIGHT = Pin(2,Pin.IN,Pin.PULL_UP) # derecha
+            self.keyUP = Pin(16,Pin.IN,Pin.PULL_UP)
+            self.keyLEFT = Pin(18,Pin.IN,Pin.PULL_UP)
+            self.keyDOWN = Pin(20,Pin.IN,Pin.PULL_UP)
+            self.keyRIGHT = Pin(2,Pin.IN,Pin.PULL_UP)
         
-        # colores codificados como RGB565 (bytes invertidos)
+        # colors coded as RGB565 (swapped bytes)
         self.red   =   0x00F8
         self.green =   0xE007
         self.blue  =   0x1F00
@@ -79,9 +80,9 @@ class LCD_1inch14(framebuf.FrameBuffer):
         self.black =   0x0000
         self.white =   0xFFFF
 
-    def brillo(self, brillo):
-        """ brillo 0-65535 """
-        self.pwm.duty_u16(brillo)
+    def set_brightness(self, brightness:int):
+        """Set LCD brightness from 0 to 65535"""
+        self.pwm.duty_u16(brightness)
     
     def write_cmd(self, cmd):
         self.cs(1)
@@ -105,13 +106,13 @@ class LCD_1inch14(framebuf.FrameBuffer):
         
         self.write_cmd(0x36)
         if self.rotation == 0:
-            # por defecto
+            # default (original Waveshare code)
             self.write_data(0x70)
         elif self.rotation == 90:
-            # rotación 270 y mirror X-Y
+            # 270 rotation and X&Y mirror
             self.write_data(0xC0)
         elif self.rotation == 180:
-            # rotación 0 y mirror X-Y
+            # 0 rotation and X&Y mirror
             self.write_data(0xB0)
         elif self.rotation == 270:
             self.write_data(0x00)
@@ -191,7 +192,7 @@ class LCD_1inch14(framebuf.FrameBuffer):
 
     def show(self):
         if self.rotation == 0:
-            # por defecto
+            # default (original Waveshare code)
             self.write_cmd(0x2A)
             self.write_data(0x00)
             self.write_data(0x28)
@@ -245,13 +246,19 @@ class LCD_1inch14(framebuf.FrameBuffer):
         self.cs(1)
         
     def off(self):
-        """ DISPOFF """
+        """DISPOFF command and brightness to 0"""
         self.write_cmd(0x28)
+        self.set_brightness(0)
     
     def on(self):
-        """ DISPON """
+        """DISPON command and restore brightness"""
         self.write_cmd(0x29)
+        self.set_brightness(self.brightness)
         
     def rgb565(self,r: int,g: int,b: int) -> int:
-        """ codifica RGB888 a RGB565 invirtiendo los bytes """
+        """Encodes RGB888 to RGB565 swapping its bytes"""
         return ((g & 0x1c) << 11) | (g >> 5) | ((b & 0xf8) << 5) | (r & 0xf8)
+    
+    def gray565(self,gray: int) -> int:
+        """Encodes 8-bit grayscale to RGB565 swapping its bytes"""
+        return ((gray & 0x1c) << 11) | (gray >> 5) | ((gray & 0xf8) << 5) | (gray & 0xf8)
